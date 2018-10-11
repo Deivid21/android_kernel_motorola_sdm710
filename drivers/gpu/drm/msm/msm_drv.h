b6d97c26421cc11864e2b330da5b503cb0008531
Bang Nguyen <bangnguyen@motorola.com>
2018-10-11 12:35:33 -0500

    Add the ACL panel param feature
    Add the ACL panel param feature which will be controlled
    from the user-space
    
    Change-Id: I1059efad9cf9af4361c198f3c682579916d35005
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1255066
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -198,6 +198,7 @@ enum msm_mdp_conn_property {
 
 	/* MOT feature panel*/
 	CONNECTOR_PROP_HBM,
+	CONNECTOR_PROP_ACL,
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
 };
@@ -286,6 +287,7 @@ enum msm_param_state {
 
 enum msm_param_id {
 	PARAM_HBM_ID = 0,
+	PARAM_ACL_ID,
 	PARAM_ID_NUM
 };
 
